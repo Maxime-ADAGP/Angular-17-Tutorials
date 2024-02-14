e3a093951a1d09f7c3476234fbb89165a949b77d
Maxime Daniel <maxime.daniel@adagp.fr>
2024-02-14 15:22:02 +0100

    Complete 'First App' tutorial of Angular

---
+++ first-app/src/app/housing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  readonly url = 'http://localhost:3000/locations';
+
+  constructor() { }
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getHousingLocationById(id: HousingLocation['id']): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    const application = { firstName, lastName, email }
+    console.log(`Homes application received: ${JSON.stringify(application)}`);
+  }
+}
